7ce8478cc340e16dc932f7f7e8edd8b0d8350dc8
Shivam Singh Sengar <shvmsengars3@gmail.com>
2021-10-05 00:24:36 +0530

    user config changes

--- config.js
+++ config.js
@@ -11,8 +11,8 @@ const CONFIG = {
   // └─┘┴ ┴└─┘┴└─┘└─┘
 
   // General
-  name: 'John',
-  imageBackground: false,
+  name: 'Ugnes',
+  imageBackground: true,
   openInNewTab: true,
   twelveHourFormat: false,
 
@@ -23,14 +23,14 @@ const CONFIG = {
   greetingNight: 'Go to Sleep!',
 
   // Weather
-  weatherKey: 'InsertYourAPIKeyHere123456',
-  weatherIcons: 'OneDark', // 'Nord', 'Dark', 'White'
+  weatherKey: 'f9b9e952631581398ce41d83875db784',
+  weatherIcons: 'Nord', // 'Nord', 'Dark', 'White'
   weatherUnit: 'C', // 'F', 'C'
   language: 'en', // More languages in https://openweathermap.org/current#multi
 
   trackLocation: true, // If false or an error occurs, the app will use the lat/lon below
-  defaultLatitude: '37.775',
-  defaultLongitude: '-122.419',
+  defaultLatitude: '12.971599',
+  defaultLongitude: '77.594566',
 
   // ┌─┐┌─┐┬─┐┌┬┐┌─┐
   // │  ├─┤├┬┘ ││└─┐
@@ -48,19 +48,19 @@ const CONFIG = {
       id: '2',
       name: 'Mail',
       icon: 'mail',
-      link: 'https://mail.protonmail.com/',
+      link: 'https://gmail.com/',
     },
     {
       id: '3',
-      name: 'Todoist',
-      icon: 'trello',
-      link: 'https://calendar.google.com/calendar/r',
+      name: 'StackOverflow',
+      icon: 'server',
+      link: 'https://stackoverflow.com/',
     },
     {
       id: '4',
-      name: 'Calendar',
-      icon: 'calendar',
-      link: 'https://calendar.google.com/calendar/r',
+      name: 'Youtube',
+      icon: 'youtube',
+      link: 'https://www.youtube.com/',
     },
     {
       id: '5',
@@ -70,9 +70,27 @@ const CONFIG = {
     },
     {
       id: '6',
-      name: 'Odysee',
-      icon: 'youtube',
-      link: 'https://odysee.com/',
+      name: 'AI StackExchange',
+      icon: 'server',
+      link: 'https://ai.stackexchange.com/',
+    },
+    {
+      id: '7',
+      name: 'GitLab',
+      icon: 'gitlab',
+      link: 'https://about.gitlab.com/',
+    },
+    {
+      id: '8',
+      name: 'LinkedIn',
+      icon: 'linkedin',
+      link: 'https://www.linkedin.com/',
+    },
+    {
+      id: '9',
+      name: 'Medium',
+      icon: 'book-open',
+      link: 'https://medium.com/',
     },
   ],
 
@@ -81,45 +99,45 @@ const CONFIG = {
   // ┴─┘┴└─┘ ┴ └─┘
 
   //Icons
-  firstListIcon: 'music',
-  secondListIcon: 'coffee',
+  firstListIcon: 'instagram',
+  secondListIcon: 'monitor',
 
   // Links
   lists: {
     firstList: [
       {
-        name: 'Inspirational',
-        link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+        name: 'LinkedIn',
+        link: 'https://www.linkedin.com/',
       },
       {
-        name: 'Classic',
-        link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+        name: 'WhatsApp',
+        link: 'https://web.whatsapp.com/',
       },
       {
-        name: 'Oldies',
-        link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+        name: 'Amazon',
+        link: 'https://www.amazon.in/',
       },
       {
-        name: 'Rock',
-        link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+        name: 'SofaScore',
+        link: 'https://www.sofascore.com/',
       },
     ],
     secondList: [
       {
-        name: 'Linkedin',
-        link: 'https://linkedin.com/',
+        name: 'StackOverflow',
+        link: 'https://stackoverflow.com/',
       },
       {
-        name: 'Figma',
-        link: 'https://figma.com/',
+        name: 'AI StackExchange',
+        link: 'https://ai.stackexchange.com/',
       },
       {
-        name: 'Dribbble',
-        link: 'https://dribbble.com',
+        name: 'Information Security',
+        link: 'https://security.stackexchange.com/',
       },
       {
-        name: 'Telegram',
-        link: 'https://webk.telegram.org',
+        name: 'Unix & Linux',
+        link: 'https://unix.stackexchange.com/',
       },
     ],
   },
